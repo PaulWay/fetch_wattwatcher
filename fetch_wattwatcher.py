6585cffef44d4ef387dc33de943aa909523f08ee
Paul Wayper <paulway@mabula.net>
2020-05-14 21:08:11 +1000

    Working complete fetch and storage of today's data
    We now have a bit more of a coherent framework for fetching data from the
    wattwatchers API and storing it either in a file or (untested) an InfluxDB
    server.  This involves rearranging the data into channel -> timestamp ->
    field, so that each channel's data can be fetched in one go rather than
    distributing it across every timestamp.  This will allow us to check local
    data timestamps in the future and only fetch missing times, as well as
    output of a channel's worth of data to other systems.
    
    Signed-off-by: Paul Wayper <paulway@mabula.net>

--- fetch_wattwatcher.py
+++ fetch_wattwatcher.py
@@ -6,7 +6,8 @@
 import argparse
 from datetime import datetime, timedelta
 from influxdb import InfluxDBClient
-from os import environ
+import json
+from os import environ, path
 import requests
 import textwrap
 import yaml
@@ -14,7 +15,7 @@ import yaml
 
 ####################
 # Constants and defaults
-config_location='/home/paulway/Coding/fetch_wattwatcher/settings.yaml'
+config_location='settings.yaml'
 
 ####################
 # Global variables
@@ -27,10 +28,14 @@ device_info = dict()
 influx_client = None
 
 
+def pwrap(*args):
+    print(textwrap.fill(*args))
+
+
 def parse_arguments():
     global args
     parser = argparse.ArgumentParser(
-        description='A Canberra Woodturner sales processor'
+        description='Fetch, manipulate and store WattWatcher Auditor data'
     )
     parser.add_argument(
         '--config-file', '-f', action='store',
@@ -67,45 +72,51 @@ def read_config():
     if 'base_url' in config['wattwatchers']:
         ww_base_url = config['wattwatchers']['base_url']
     if not (ww_api_key and ww_api_key.startswith('key_')):
-        print(textwrap.fill(
+        pwrap(
             "Error: WattWatchers API key not found or does not start "
             "with 'key_'.  Set via config file under wattwatchers.api_key "
             "or use environment variable WATTWATCHERS_API_KEY."
-        ))
+        )
         exit(1)
     ww_headers = {
         'Authorization': 'Bearer ' + ww_api_key
     }
-    
+
     pv_api_key = environ.get('PVOUTPUT_API_KEY', config['pvoutput']['api_key'])
     if not (pv_api_key and len(pv_api_key) == 40):
-        print(textwrap.fill(
+        pwrap(
             "Error: PVOutput API key not found or not correct length. "
             "Set via config file under pvoutput.api_key "
             "or use environment variable PVOUTPUT_API_KEY."
-        ))
+        )
         exit(1)
     if 'system_id' not in config['pvoutput']:
         print("Error: PVOutput System ID not set in 'system_id' key in pvoutput config")
         exit(1)
 
-    if 'influxdb' in config:
-        influx_host = config['influxdb'].get('host', 'localhost')
-        influx_port = int(config['influxdb'].get('port', '8086'))
-        influx_user = config['influxdb'].get('username', 'influxdb')
-        influx_pass = config['influxdb'].get('password', 'influxdb')
-        influx_database = config['influxdb'].get('database', 'energy_data')
-        global influx_client
-        influx_client = InfluxDBClient(
-            host=influx_host, port=influx_port,
-            username=influx_user, password=influx_pass,
-        )
-        if influx_database not in (
-            db['name'] for db in influx_client.get_list_database()
-        ):
-            influx_client.create_database(influx_database)
-        influx_client.switch_database(influx_database)
-            
+
+def set_up_influxdb(influx_config):
+    """
+    Start the connection to the InfluxDB server, if necessary.
+    """
+    global influx_client
+    if influx_client is not None:
+        return
+    influx_host = influx_config.get('host', 'localhost')
+    influx_port = int(influx_config.get('port', '8086'))
+    influx_user = influx_config.get('username', 'influxdb')
+    influx_pass = influx_config.get('password', 'influxdb')
+    influx_database = influx_config.get('database', 'energy_data')
+    influx_client = InfluxDBClient(
+        host=influx_host, port=influx_port,
+        username=influx_user, password=influx_pass,
+    )
+    if influx_database not in (
+        db['name'] for db in influx_client.get_list_database()
+    ):
+        influx_client.create_database(influx_database)
+    influx_client.switch_database(influx_database)
+
 
 def ww_api_get(endpoint, parameters=None):
     """
@@ -118,7 +129,7 @@ def ww_api_get(endpoint, parameters=None):
         headers=ww_headers
     )
     if response.status_code != 200:
-        print(f"Error: GET {endpoint} returned status {response.status_code} - {response.data}")
+        pwrap(f"Error: GET {endpoint} returned status {response.status_code} - {response.data}")
         exit(1)
     # print(f"GET {endpoint} returned {response.status_code}: {response.json()}")
     return response.json()
@@ -148,10 +159,10 @@ def device_channel_data(device, channel):
     if isinstance(channel, int):
         # Look for channel by number, starting from channel 1
         if channel - 1 > len(device_info[device]['channels']):
-            print(textwrap.fill(
+            pwrap(
                 f"Warning: channel {channel} not found in channels of "
                 f"{device}: {len(device_info[device]['channels'])} known"
-            ))
+            )
             return {}
         channel_data = device_info[device]['channels'][channel + 1]
     elif isinstance(channel, str):
@@ -160,11 +171,11 @@ def device_channel_data(device, channel):
             if channel_data['label'] == channel:
                 break
         else:
-            print(textwrap.fill(
+            pwrap(
                 f"Warning: could not find channel labelled '{channel}' "
                 f"in device {device} - we know about "
                 f"{', '.join(chan['label'] for chan in device_info[device]['channels'])}"
-            ))
+            )
             return {}
     return channel_data
 
@@ -175,16 +186,16 @@ def get_device_daily_usage(device, resolution='5m'):
     resolution (which can be one of '5m', '15m', '30m', or 'hour').
     """
     if device not in device_info:
-        print(textwrap.fill(
+        pwrap(
             f"Warning: device {device} not found in data from "
             f"WattWatchers - we know about {', '.join(device_info.keys())}"
-        ))
+        )
         return None
     valid_resolutions = ('5m', '15m', '30m', 'hour')
     if resolution not in valid_resolutions:
-        print(textwrap.fill(
+        pwrap(
             f"Warning: resolution should be one of {valid_resolutions}"
-        ))
+        )
         return None
     now = datetime.now()
     # Probably a good point here to fetch from InfluxDB which samples
@@ -195,43 +206,172 @@ def get_device_daily_usage(device, resolution='5m'):
         'long-energy/' + device,
         {'fromTs': start, 'toTs': finit, 'granularity': resolution}
     )
+    # Data comes back
     print("Got usage data", usage_data, "for device", device)
     return usage_data
 
-def store_usage_data_in_influxdb(device, usage_data)
-    channel_names = [
-        channel['label']
-        for channel in device_info[device]['channels']
-    ]
-    # We want to submit each channel as a tag (indexed), and its fields
-    # are the values reported.  So we have to reorganise this table.
-    def sample_to_fields(sample, pos):
-        return {
-            key: val[pos]
-            for key, val in sample.items()
-            if isinstance(val, list)
+
+def sample_to_fields(sample, pos):
+    """
+    Each sample in the long data is in the form:
+    {
+        "timestamp": 1550408400,
+        "duration": 900,
+        "eReal": [
+            3639405, 336898, -560, 37, -14, -25
+        ],
+        ...
+    }
+    When we're compiling the data for one channel, we want to get that
+    vertical 'slice' of samples from all fields that are lists of
+    samples.  So we take a sample, and the position to slice through,
+    and return (e.g. for position 1):
+    {
+        'eReal': 336898, 'eRealNegative': 0, 'eRealPositive': 336898,
+        'eReactive': 140216, ...
+    }
+    Only fields that have a list value are captured in this slice.
+    """
+    return {
+        key: val[pos]
+        for key, val in sample.items()
+        if isinstance(val, list)
+    }
+
+
+def get_daily_usage(resolution='5m'):
+    """
+    Get the power usage from all devices for today, at the given
+    resolution (which can be one of '5m', '15m', '30m', or 'hour').
+    The result is combined with metadata of the start and end time for this
+    day.  The results are stored per channel across all devices and are
+    rearranged into 'channel' -> 'timestamp' -> 'value' - something like this:
+
+    {
+      "House": {
+        "1584492900": {
+          "eReal": 66564,
+          "eRealNegative": 0,
+          "eRealPositive": 66564,
+          "eReactive": -1215,
+          "eReactiveNegative": 4800,
+          "eReactivePositive": 3585,
+          "vRMSMin": 244.1,
+          "vRMSMax": 244.9,
+          "iRMSMin": 0.761,
+          "iRMSMax": 1.074
+        },
+        "1584493200": {
+          "eReal": 67548,
+          "eRealNegative": 0,
+          "eRealPositive": 67548,
+          "eReactive": 4068,
+          "eReactiveNegative": 0,
+          "eReactivePositive": 4068,
+          "vRMSMin": 244,
+          "vRMSMax": 244.7,
+          "iRMSMin": 0.798,
+          "iRMSMax": 0.97
+        },
+      ...
+    }
+
+    Data from the WattWatchers devices are stored in Joules.  Since one
+    kilowatt-hour is 3600000 joules, this provides better precision in
+    integer format.
+
+    """
+    valid_resolutions = ('5m', '15m', '30m', 'hour')
+    if resolution not in valid_resolutions:
+        pwrap(
+            f"Warning: resolution should be one of {valid_resolutions}"
+        )
+        return None
+    now = datetime.now()
+    # Probably a good point here to fetch from InfluxDB which samples
+    # we've already collected, and update our start timestamp.
+    start = int(now.replace(hour=0, minute=0, second=0).timestamp())
+    finit = int(now.replace(hour=23, minute=59, second=59).timestamp())
+    usage_data = {  # otherwise organised by timestamp to combine channels
+        '_metadata_': {
+            'start_ts': start, 'finish_ts': finit, 'granularity': resolution
         }
+    }
+    for device, device_data in device_info.items():
+        device_usage_data = ww_api_get(
+            'long-energy/' + device,
+            {'fromTs': start, 'toTs': finit, 'granularity': resolution}
+        )
+        # Data comes back in lists, one item per channel; we want to
+        # rearrange that into usage[channel][timestamp][field] - since
+        # the channel shouldn't already be in there.
+        for fnum, channel_data in enumerate(device_data['channels']):
+            channel = channel_data['label']
+            if channel in device_usage_data:
+                pwrap(
+                    f"Warning: channel named 'channel' appears twice in "
+                    f"device data - rename one of them.  Skipping the one "
+                    f"on device '{device}'."
+                )
+                continue
+            usage_data[channel] = {
+                sample['timestamp']: sample_to_fields(sample, fnum)
+                for sample in device_usage_data
+            }
+            # Store device name in channel metadata
+            usage_data[channel]['_metadata_'] = {
+                'device': device
+            }
+    print("Got usage data - keys:", usage_data.keys())
+    return usage_data
+
+
+def store_usage_data_in_data_dir(usage_data, data_dir):
+    """
+    Store the usage data we've collected in our data directory.
+
+    Each file is named 'wattwatcher_{start_ts}-{finish_ts}-{granularity}.json
+    - e.g. wattwatcher_1550408400_1550494800_5m.json
+    """
+    meta = usage_data['_metadata_']
+    filename = f"wattwatcher_{meta['start_ts']}-{meta['finish_ts']}-{meta['granularity']}.json"
+    with open(path.join(data_dir, filename), 'w') as fh:
+        json.dump(usage_data, fh)
+
+
+def store_usage_data_in_influxdb(usage_data):
+    """
+    Store the usage data in InfluxDB.
+    """
+    assert 'influxdb' in config, 'Config does not contain an influxdb section'
+    # Mostly just arranging the tags and avoiding outputting metadata
     data_points = [
         {
             'measurement': 'energy_data',
             'tags': {
-                'device': device,
+                'device': channel['_metadata_']['device'],
                 'channel': channel,
             },
-            'fields': sample_to_fields(sample, pos),
-            'time': sample['timestamp']
+            'fields': time_data,
+            'time': timestamp
         }
-        for pos, channel in enumerate(channel_names)
-        for sample in usage_data
+        for channel, channel_data in usage_data.items()
+        for timestamp, time_data in channel_data
+        if channel != '_metadata_' and timestamp != '_metadata_'
     ]
     print("Data points mangled into:", data_points)
+    set_up_influxdb(config['influxdb'])
     influxdb.write_points(data_points, time_precision='s')
+
 
 if __name__ == '__main__':
     parse_arguments()
     read_config()
     get_devices()
-    if 'pvoutput' in config and 'wattwatcher_device' in config['pvoutput'] and 'wattwatcher_channel' in config['pvoutput']:
-        usage_data = get_device_daily_usage(config['pvoutput']['wattwatcher_device'])
-        if 'influxdb' in config:
-            store_usage_data_in_influxdb(config['pvoutput']['wattwatcher_device'], usage_data)
+    # usage = get_device_daily_usage('ED055CC1AF588')
+    usage_data = get_daily_usage()
+    if 'storage' in config and 'data_dir' in config['storage']:
+        store_usage_data_in_data_dir(usage_data, config['storage']['data_dir'])
+    if 'influxdb' in config and config['influxdb'].get('enabled', 'true') == 'true':
+        store_usage_data_in_influxdb(usage_data)
+    # if 'pvoutput' in config and 'wattwatcher_device' in config['pvoutput'] and 'wattwatcher_channel' in config['pvoutput']:
